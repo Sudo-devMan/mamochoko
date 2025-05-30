4be1c22af0386829a8f191c89a5c0f0f13f4cbf4
Sudo-devMan <inevitabledouble.e@gmail.com>
2025-05-30 05:05:48 +0200

    Update settings.py

--- mams/settings.py
+++ mams/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-83%_jxtae9!owrq0gq%1(vhryj3-v=va&0(#h!@alk@8qb(s*n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
